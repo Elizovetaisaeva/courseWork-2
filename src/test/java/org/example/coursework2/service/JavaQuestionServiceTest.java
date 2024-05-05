b6e2de3bcf3f4448cf3daab9fc47c9108ba6b8f2
Лаисат <lz_256@mail.ru>
2024-05-05 21:45:18 +0300

    comme

--- src/test/java/org/example/coursework2/service/JavaQuestionServiceTest.java
+++ src/test/java/org/example/coursework2/service/JavaQuestionServiceTest.java
@@ -11,12 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.OngoingStubbing;
 
-import java.util.Iterator;
-import java.util.List;
-
-import static java.util.List.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
