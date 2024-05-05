80be3b1a90b7b615099d5aa90d85ddddad616c1d
Лаисат <lz_256@mail.ru>
2024-05-05 23:16:10 +0300

    end

--- src/main/java/org/example/coursework2/service/impl/ExaminerServiceImpl.java
+++ src/main/java/org/example/coursework2/service/impl/ExaminerServiceImpl.java
@@ -17,15 +17,13 @@ import java.util.stream.Stream;
 public class ExaminerServiceImpl implements ExaminerService {
     private static final Random random = new Random();
 
-    private final QuestionService javaQuestionService;
-    private final QuestionService mathQuestionService;
+    private final List<QuestionService> questionServices;
 
 
-    public ExaminerServiceImpl(@Qualifier("javaQuestionService") QuestionService javaQuestionService,
-                               @Qualifier("mathQuestionService") QuestionService mathQuestionService) {
 
-        this.javaQuestionService =javaQuestionService;
-        this.mathQuestionService = mathQuestionService;
+    public ExaminerServiceImpl(List<QuestionService> questionServices) {
+        this.questionServices = questionServices;
+
     }
 
     @Override
@@ -35,20 +33,17 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     @Override
     public Collection<Question> getquestion(int amount) {
-        if (javaQuestionService.getAll().size() + mathQuestionService.getAll().size()<amount) {
-            throw new QuestionsAmoutExceededException();
-        }
-        return Stream.generate(this::getRandomQuestion)
+                return Stream.generate(this::getRandomQuestion)
                 .distinct()
                 .limit(amount)
                 .collect(Collectors.toList());
     }
     private Question getRandomQuestion() {
-        int ind = random.nextInt(2);
-        if (ind == 0) {
-            return javaQuestionService.getRandom();
-        } else {
-            return mathQuestionService.getRandom();
+        int ind = random.nextInt(questionServices.size());
+        return questionServices.get(ind).getRandom();
+
         }
+
+
     }
-}
+
--- src/main/java/org/example/coursework2/service/impl/MathQuestionService.java
+++ src/main/java/org/example/coursework2/service/impl/MathQuestionService.java
@@ -1,8 +1,8 @@
 package org.example.coursework2.service.impl;
 
 import org.example.coursework2.entity.Question;
+import org.example.coursework2.exception.NotImplementedException;
 import org.example.coursework2.repository.QuestionRepository;
-import org.example.coursework2.repository.impl.MathQuestionRepository;
 import org.example.coursework2.service.QuestionService;
 import org.springframework.stereotype.Service;
 
@@ -14,40 +14,46 @@ public class MathQuestionService implements QuestionService {
 
     private final Random random = new Random();
 
-    private QuestionRepository questionRepository;
-
-    public MathQuestionService(MathQuestionRepository questionRepository) {
-        this.questionRepository = questionRepository;
-    }
 
     @Override
     public Question add(Question question) {
-        return questionRepository.aaddQuestion(question);
+        throw new NotImplementedException();
 
     }
 
     @Override
     public Question add(String question, String answer) {
-        return questionRepository.aaddQuestion(new Question(question, answer));
+        throw new NotImplementedException();
 
     }
 
     @Override
     public Question remove(Question question) {
-        return questionRepository.removeQuestion(question);
+        throw new NotImplementedException();
 
     }
 
     @Override
     public Collection<Question> getAll() {
-
-        return questionRepository.getAll();
+        throw new NotImplementedException();
     }
 
     @Override
     public Question getRandom() {
-        int ind = random.nextInt(questionRepository.getAll().size());
-        return questionRepository.getAll().get(ind);
+        int a = random.nextInt(100);
+        int b = random.nextInt(100);
+        int operation = random.nextInt(4);
+        switch (operation) {
+            case 0 -> new Question(a + "+" + b, String.valueOf(a + b));
+            case 1 -> new Question(a + "-" + b, String.valueOf(a - b));
+            case 2 -> new Question(a + "*" + b, String.valueOf(a * b));
+            case 3 -> new Question(a + "/" + b, String.valueOf(a / b));
+            default -> throw new IllegalStateException();
 
+        };
+        return null;
     }
 }
+
+
+
